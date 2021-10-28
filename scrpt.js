32c66ce02ae9ba693f8ef2167e3673108af185e8
Nikola <nikola.dionisiev@gmail.com>
2021-10-28 15:32:49 +0300

    fixed about section text and added sticky header

--- scrpt.js
+++ scrpt.js
@@ -1,3 +1,22 @@
 function chbg(id, color) {
     document.getElementById(id).style.backgroundColor = color;
 }
+
+
+//STICKY HEADER SCROLL
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+var scrollPos = 0;
+
+window.addEventListener('scroll', function(){
+
+if ((document.body.getBoundingClientRect()).top > scrollPos){
+    header.classList.add("sticky");
+}
+else{
+    header.classList.remove("sticky");
+}
+    scrollPos = (document.body.getBoundingClientRect()).top;
+});
+
+//================================================================
